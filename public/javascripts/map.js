cf48e4fc9165c1dbd694123b583e553814183b03
Aditya Godara <devcore3d@outlook.com>
2016-02-10 04:27:32 +0000

    code formatting map.js

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,108 +1,108 @@
 function LocationMap(client) {
-	var location = {
-		lat: 51.505,
-		lng: -0.09
-	};
-	
-	var maxZoom = 7;
-	var map = L.map('map').locate({
-		setView: true
-	});
+    var location = {
+        lat: 51.505,
+        lng: -0.09
+    };
 
-	map.on('dblclick', function(e) {
-		location = e.latlng;
-		setView();
-		client.getTrends(location, showTrends, showError);
-	});
+    var maxZoom = 7;
+    var map = L.map('map').locate({
+        setView: true
+    });
 
-	map.on('locationfound', geoSuccess);
-	map.on('locationerror', geoError);
+    map.on('dblclick', function (e) {
+        location = e.latlng;
+        setView();
+        client.getTrends(location, showTrends, showError);
+    });
 
-	function geoSuccess(position) {
-		location = position.latlng;
-		client.getTrends(location, showTrends, showError);
-	}
+    map.on('locationfound', geoSuccess);
+    map.on('locationerror', geoError);
 
-	function geoError() {
-		//askWhereAreYou();
-		client.getTrends(location, showTrends, showError);
-	}
+    function geoSuccess(position) {
+        location = position.latlng;
+        client.getTrends(location, showTrends, showError);
+    }
 
-	function askWhereAreYou() {
-		var result = prompt("Where are you?");
-	}
+    function geoError() {
+        //askWhereAreYou();
+        client.getTrends(location, showTrends, showError);
+    }
 
-	function showTrends(trends) {
-		popup(formatTrends(trends));
-	}
-	
-	function showError(xhr, message, error) {
-		console.error(message);
-		console.error(error);
-		popup('<span class="error">Sorry! Something went wrong.</span>');
-	}
-	
-	function formatTrends(trends) {
-		function formatTrend(trend) {
-			return "<a href=" + trend.url + ">" + trend.name + "</a>";
-		}
+    function askWhereAreYou() {
+        var result = prompt("Where are you?");
+    }
 
-		trends = trends.length > 5 && trends.slice(0, 5);
-		
-		var htmlContent = "";
-		for (var i = 0; i < trends.length; i++) {
-			htmlContent += formatTrend(trends[i]) + "<br />";
-		}
+    function showTrends(trends) {
+        popup(formatTrends(trends));
+    }
 
-		return htmlContent;
-	}
+    function showError(xhr, message, error) {
+        console.error(message);
+        console.error(error);
+        popup('<span class="error">Sorry! Something went wrong.</span>');
+    }
 
-	function popup(content) {
-		var marker = L.marker(location).addTo(map);
-		marker.bindPopup(content).openPopup();
-	}
+    function formatTrends(trends) {
+        function formatTrend(trend) {
+            return "<a href=" + trend.url + ">" + trend.name + "</a>";
+        }
 
-	function setView() {
-		map.setView(location, maxZoom);
-	}
+        trends = trends.length > 5 && trends.slice(0, 5);
 
-	function renderMap() {
-		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-			attribution : 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-			maxZoom: maxZoom,
-			id : MapBox.id,
-			accessToken : MapBox.accessToken
-		}).addTo(map);
-		setView();
-	}
+        var htmlContent = "";
+        for (var i = 0; i < trends.length; i++) {
+            htmlContent += formatTrend(trends[i]) + "<br />";
+        }
 
-	return {
-		render: renderMap
-	}
-};
+        return htmlContent;
+    }
+
+    function popup(content) {
+        var marker = L.marker(location).addTo(map);
+        marker.bindPopup(content).openPopup();
+    }
+
+    function setView() {
+        map.setView(location, maxZoom);
+    }
+
+    function renderMap() {
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: maxZoom,
+            id: MapBox.id,
+            accessToken: MapBox.accessToken
+        }).addTo(map);
+        setView();
+    }
+
+    return {
+        render: renderMap
+    }
+}
 
 function TwitterClient() {
-	var successCallback;
-	var errorCallback;
+    var successCallback;
+    var errorCallback;
 
-	function getClosestLocation(location, onSuccess, onError) {
-		var closestLocationRoute = jsRoutes.controllers.ApplicationController.locations(location.lat, location.lng)
-		$.ajax(closestLocationRoute.url).success(onSuccess).error(onError);
-	}
+    function getClosestLocation(location, onSuccess, onError) {
+        var closestLocationRoute = jsRoutes.controllers.ApplicationController.locations(location.lat, location.lng)
+        $.ajax(closestLocationRoute.url).success(onSuccess).error(onError);
+    }
 
-	function trendsAjaxCall(data) {
-		var trendsRoute = jsRoutes.controllers.ApplicationController.trends(data[0].woeid);
-		$.ajax(trendsRoute.url).success(successCallback).error(errorCallback);
-	}
-	
-	return {
-		getTrends: function (location, onSuccess, onError) {
-			successCallback = onSuccess;
-			errorCallback = onError;
-			getClosestLocation(location, trendsAjaxCall, errorCallback);
-		}
-	}
-};
+    function trendsAjaxCall(data) {
+        var trendsRoute = jsRoutes.controllers.ApplicationController.trends(data[0].woeid);
+        $.ajax(trendsRoute.url).success(successCallback).error(errorCallback);
+    }
+
+    return {
+        getTrends: function (location, onSuccess, onError) {
+            successCallback = onSuccess;
+            errorCallback = onError;
+            getClosestLocation(location, trendsAjaxCall, errorCallback);
+        }
+    }
+}
 
 var locationMap = new LocationMap(new TwitterClient());
 
